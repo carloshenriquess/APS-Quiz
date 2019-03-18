a63d83f67fc2f293de42273d91274932419964af
Carlos Henrique <carlos.henrique0307200@gmail.com>
2019-03-17 21:39:59 -0300

    Início da estrutura

---
+++ src/teste/Teste.java
@@ -0,0 +1,11 @@
+package teste;
+
+import view.Menu;
+
+public class Teste {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.setVisible(true);
+    }
+    
+}
